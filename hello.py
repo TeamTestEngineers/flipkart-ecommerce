2139f238fad851057dfd4c68b3ae3550c3d34137
TeamTestEngineers <numerouno51@gmail.com>
2021-12-30 19:19:06 +0530

    new build

--- hello.py
+++
@@ -1,7 +0,0 @@
-print('Hello world')
-i= 3
-print(i)
-j= i+1
-print(j)
-print(i+j)
-
---
+++ variables/hello.py
@@ -0,0 +1,13 @@
+print('Hello world')
+i= 3
+print(i)
+j= i+1
+print(j)
+print(i+j)
+dog_name ='spot'
+print(dog_name)
+dog_name= 'rex'
+print(dog_name)
+dog_name = 't-'+dog_name
+print(dog_name)
+
